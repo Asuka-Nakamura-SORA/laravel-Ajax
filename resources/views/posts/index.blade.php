3088a3d05890e056c27e85cb0657b591a0fbcc5e
asuasuasuasuasuasu <a.nakamur@so-ra.co.jp>
2024-05-09 14:48:31 +0900

    firstcommit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,28 +12,48 @@
 <body>
 <div class="background">
     <h1><a href="{{ route('posts.index') }}">Laravel News</a></h1> {{-- index.blade.phpへのリンク --}}
-    <form id="postForm">
+    <form action="{{ route('posts.store')}}" method="POST" id="postForm">
         @csrf
 
         <label for="title">タイトル:</label>
         <input type="text" name="title" id="title">
-        <div id="titleError"></div>
+        @error('title')
+            <div class="alert">{{ $message }}</div>
+        @enderror
 
         <label for="message">投稿内容:</label>
         <textarea name="message" id="message"></textarea>
-        <div id="messageError"></div>
+        @error('message')
+            <div class="alert">{{ $message }}</div>
+        @enderror
 
         <button type="submit">投稿する</button>
-
     </form>
 
+    @if ($errors->any())
+        {{-- エラーメッセージ --}}
+        <div class="error">
+            {!! implode('<br>', $errors->all()) !!}
+        </div>
+        @elseif (session()->has('success'))
+        {{-- 成功メッセージ --}}
+        <div class="success">
+            {{ session()->get('success') }}
+        </div>
+    @endif
+
     <h2>ニュース一覧</h2>
-    <div id="newsList">
-    @foreach ($posts as $post)
-        <h3><a href="{{ route('posts.show', $post->id) }}">タイトル：{{ $post->title }}</a></h3>
-        <p>内容：{{ $post->message }}</p>
+<div id="newsList">
+    @foreach ($posts as $post) {{-- PostControllerのindexメソッド内の「$posts」を受け取る --}}    
+        <h3><a href="{{ route('posts.show', $post->id) }}">タイトル：{{ $post->title }}</h3>
+                <div id="titleError"></div>
+        内容：{{ Str::limit( $post->message , 20,'...') }}</p>
+        <div id="messageError"></div>
+        <td><a href="{{ route('posts.show', $post->id) }}">詳しく見せるよん<br><br></a></td>
+        <br>
     @endforeach
-    </div>
+    
+</div>
 
 <script>
     $.ajaxSetup({
@@ -61,6 +81,7 @@
             }
         });
     });
+
     success: function(response) {
         $('#title').val('');
         $('#message').val('');
@@ -69,6 +90,8 @@
         var newPostHtml = '<h3><a href="' + '/posts/' + response.post.id + '">タイトル：' + response.post.title + '</a></h3><p>内容：' + response.post.message + '</p>';
         $('#newsList').prepend(newPostHtml);
 }
+
 </script>
+
 </body>
 </html>
