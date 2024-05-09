9b983bb96d46c208faa70e7fc81ec30f7a781740
asuasuasuasuasuasu <a.nakamur@so-ra.co.jp>
2024-05-09 15:42:37 +0900

    ok

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,47 +12,34 @@
 <body>
 <div class="background">
     <h1><a href="{{ route('posts.index') }}">Laravel News</a></h1> {{-- index.blade.phpへのリンク --}}
-    <form action="{{ route('posts.store')}}" method="POST" id="postForm">
-        @csrf
-
-        <label for="title">タイトル:</label>
-        <input type="text" name="title" id="title">
-        @error('title')
-            <div class="alert">{{ $message }}</div>
-        @enderror
-
-        <label for="message">投稿内容:</label>
-        <textarea name="message" id="message"></textarea>
-        @error('message')
-            <div class="alert">{{ $message }}</div>
-        @enderror
-
-        <button type="submit">投稿する</button>
+    <form id="postForm">
+        <div class="form">
+            @csrf
+            <p>タイトル</p>
+            <input type="text" name="title" id="title"/>
+            <div id="titleError"></div>
+            <br/>
+            <p>本文</p>
+            <textarea name="message" id="message"></textarea>
+            <div id="messageError"></div>
+            <br/>         
+        </div>
+        <div class="buttun">
+            <input type="submit" value="投稿">
+        </div>
+        <h3>一覧</h3>
     </form>
 
-    @if ($errors->any())
-        {{-- エラーメッセージ --}}
-        <div class="error">
-            {!! implode('<br>', $errors->all()) !!}
-        </div>
-        @elseif (session()->has('success'))
-        {{-- 成功メッセージ --}}
-        <div class="success">
-            {{ session()->get('success') }}
-        </div>
-    @endif
+    <div id="newsList">
+        @foreach ($posts as $post)
+            <p>タイトル：{{ $post->title }}</p>
+            <p>内容：{{ Str::limit( $post->message , 20,'...') }}</p>
+            <a href="{{ route('posts.show', $post->id) }}">詳しく見せるよん<br><br></a>
+        @endforeach
+    </div>
 
-    <h2>ニュース一覧</h2>
-<div id="newsList">
-    @foreach ($posts as $post) {{-- PostControllerのindexメソッド内の「$posts」を受け取る --}}    
-        <h3><a href="{{ route('posts.show', $post->id) }}">タイトル：{{ $post->title }}</h3>
-                <div id="titleError"></div>
-        内容：{{ Str::limit( $post->message , 20,'...') }}</p>
-        <div id="messageError"></div>
-        <td><a href="{{ route('posts.show', $post->id) }}">詳しく見せるよん<br><br></a></td>
-        <br>
-    @endforeach
     
+
 </div>
 
 <script>
@@ -73,24 +60,24 @@
                     data: $(this).serialize(),
                     success: function(response) {
                         $('#title').val('');
-                        $('#message').val('');                   
-        }               
-    });
+                        $('#message').val('');
+                        $('#titleError').empty();
+                        $('#messageError').empty();
+                        var newPostHtml = '<h3><a href="' + '/posts/' + response.post.id + '">タイトル：' + response.post.title + '</a></h3><p>内容：' + response.post.message + '</p>';
+                        $('#newsList').prepend(newPostHtml);                   
+                    }    ,
+                    error: function(xhr) {
+                        var errors = xhr.responseJSON.errors;
+                        $('#titleError').text(errors.title && errors.title[0]);
+                        $('#messageError').text(errors.message && errors.message[0]);
+                    }
+                
+                });
             } else {
                 return false;
             }
         });
     });
-
-    success: function(response) {
-        $('#title').val('');
-        $('#message').val('');
-
-     // 投稿データから詳細ページへのリンクを生成
-        var newPostHtml = '<h3><a href="' + '/posts/' + response.post.id + '">タイトル：' + response.post.title + '</a></h3><p>内容：' + response.post.message + '</p>';
-        $('#newsList').prepend(newPostHtml);
-}
-
 </script>
 
 </body>
